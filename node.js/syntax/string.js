45594f0f6ce18de4f70256c76fbcf7b9f9b12db1
2019094957 <world2013@naver.com>
2020-09-06 02:10:32 +0900

    day2

---
+++ node.js/syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');
+console.log('dsfgdfg dsfgfdgdfhyjf ijoads ljk ;oj'.length);
---
+++ node.js/syntax/template.js
@@ -0,0 +1,4 @@
+var name = 'hanmin';
+var letter = `Dear ${name} \n\n hi I'm ${name} ${1+1}`;
+
+console.log(letter); 
